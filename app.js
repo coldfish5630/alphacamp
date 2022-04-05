09ba6a79a5807005d40c010d07734ddeedae2bc8
cuisine <layan.schicksal@gmail.com>
2022-04-05 17:32:55 +0900

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
+require('dotenv').config()
+// console.log(process.env)
+
 const port = 3000
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongoDB error')
+})
+
+db.once('open', () => {
+  console.log('mongoDB connected')
+})
 
 app.get('/', (req, res) => {
   res.send(`<h1>this is a to do list</h1>`)
