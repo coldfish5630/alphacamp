72feab73efeb358b387d5a7586383524e9b781d1
cuisine <layan.schicksal@gmail.com>
2022-04-14 00:01:55 +0900

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
 const express = require('express')
-const mongoose = require('mongoose')
-const Todo = require('./models/todo')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 const app = express()
 
-require('dotenv').config()
+require('./config/mongoose')
 // console.log(process.env)
 
 const port = 3000
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongoDB error')
-})
-
-db.once('open', () => {
-  console.log('mongoDB connected')
-})
 
 const exphbs = require('express-handlebars')
 
