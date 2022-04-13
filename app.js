50e345bc069d17bf89a7b380a9321cc1453490ee
cuisine <layan.schicksal@gmail.com>
2022-04-13 23:04:12 +0900

    feat: add method-override

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Todo = require('./models/todo')
+const methodOverride = require('method-override')
 const app = express()
 
 require('dotenv').config()
@@ -29,6 +30,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   Todo.find()
@@ -65,7 +67,7 @@ app.get('/todos/:id/edit', (req, res) => {
     .catch(error => console.error(error))
 })
 
-app.post('/todos/:id/edit', (req, res) => {
+app.put('/todos/:id', (req, res) => {
   const id = req.params.id
   const { name, isDone } = req.body
   return Todo.findById(id)
@@ -78,7 +80,7 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.error(error))
 })
 
-app.post('/todos/:id/delete', (req, res) => {
+app.delete('/todos/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .then(todo => todo.remove())
