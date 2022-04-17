fd29bdde4e1a8a0303c4d1cccc419adf0bd5f4a0
cuisine <layan.schicksal@gmail.com>
2022-04-17 14:48:42 +0900

    feat: add bootstrap & modify main.hbs

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,18 +3,18 @@ const router = express.Router()
 
 const Todo = require('../../models/todo')
 
-router.get('/todos/new', (req, res) => {
+router.get('/new', (req, res) => {
   return res.render('new')
 })
 
-router.post('/todos', (req, res) => {
+router.post('/', (req, res) => {
   const name = req.body.name
   return Todo.create({ name })
     .then(() => res.redirect('/'))
     .catch(error => console.error(error))
 })
 
-router.get('/todos/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .lean()
@@ -22,7 +22,7 @@ router.get('/todos/:id', (req, res) => {
     .catch(error => console.error(error))
 })
 
-router.get('/todos/:id/edit', (req, res) => {
+router.get('/:id/edit', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .lean()
@@ -30,7 +30,7 @@ router.get('/todos/:id/edit', (req, res) => {
     .catch(error => console.error(error))
 })
 
-router.put('/todos/:id', (req, res) => {
+router.put('/:id', (req, res) => {
   const id = req.params.id
   const { name, isDone } = req.body
   return Todo.findById(id)
@@ -39,11 +39,11 @@ router.put('/todos/:id', (req, res) => {
       todo.isDone = isDone === 'on'
       return todo.save()
     })
-    .then(() => res.redirect(`/todos/${id}`))
+    .then(() => res.redirect(`/${id}`))
     .catch(error => console.error(error))
 })
 
-router.delete('/todos/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .then(todo => todo.remove())
