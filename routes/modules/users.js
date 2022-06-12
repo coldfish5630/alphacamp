eaf3d61c265847fc56333b933b0cdebc6c2961a1
coldfish5630 <layan.schicksal@gmail.com>
2022-06-12 09:45:29 +0900

    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const User = require('../../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -56,7 +57,10 @@ router.post('/register', (req, res) => {
           confirmPassword
         })
       } else {
-        return User.create({ name, email, password })
+        return bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(password, salt))
+          .then(hash => User.create({ name, email, password: hash }))
           .then(() => res.redirect('/'))
           .catch(err => console.log(err))
       }
